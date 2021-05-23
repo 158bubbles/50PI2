ee7024d65259ee01a327c8d8864d54ef39b321a8
158bubbles <49231263+158bubbles@users.noreply.github.com>
2021-05-23 02:05:09 +0100

    Create 19.c

---
+++ 19.c
@@ -0,0 +1,22 @@
+int take (int n, LInt *head){
+    
+    int i;
+    LInt current = *head;
+    
+    //enquanto nao chega ao elemento nº n avança so na lista
+    for(i=0; i+1 < n && current != NULL;i++){
+        current = current->prox;
+    }
+    
+    //se saiu do ciclo pq chegou a current == NULL
+    if(current == NULL){
+        return i;
+    }
+    
+    //se saiu do ciclo pq chegou à altura de apagarmos os nodos para lá do n
+    LInt removidos = current->prox;   //criamos uma LInt para agrupar os nodos a remover, que sao sempre os a seguir do current em que estva ao sair do ciclo
+    current->prox = NULL;
+    free(removidos);
+    
+    return i+1;
+}
