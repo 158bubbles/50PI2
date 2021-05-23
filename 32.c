7d664c2c9aefc7251054da7529ae9e15a3ff1409
158bubbles <49231263+158bubbles@users.noreply.github.com>
2021-05-23 17:22:57 +0100

    Create 32.c

---
+++ 32.c
@@ -0,0 +1,29 @@
+void acrescenta(int x, LInt *head){
+    
+    LInt current = *head;
+    LInt novo = newLInt(x, NULL);  //temos de criar este nodo com o x p o acrescentar
+    
+    if(*head == NULL){     //se nao tiver nada ainda da parte esq da arvore
+        *head = novo;
+        return;
+    }
+    
+    while(current->prox != NULL){
+        current = current->prox;
+    }
+    
+    //quando chegarmos ao ultimo elemento da head acrescentamos a root
+    current->prox = novo;
+    
+}
+
+void inorder (ABin root, LInt *head) {
+    
+    if(root != NULL){
+        
+        inorder(root->esq, head);
+        acrescenta(root->valor, head);
+        inorder(root->dir, head);
+    }
+    
+}
