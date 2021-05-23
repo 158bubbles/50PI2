f3e304b708b55d075e9b7f446dfd7a95aa86541c
158bubbles <49231263+158bubbles@users.noreply.github.com>
2021-05-23 18:24:18 +0100

    Update 35.c

--- 35.c
+++ 35.c
@@ -4,9 +4,9 @@ int freeAB (ABin root) {
         return 0;
     } else {
         
-        ABin esq = root->esq;
+        ABin esq = root->esq;    //temos que guardar os apontadores antes de libertar o espaço
         ABin dir = root->dir;
-        free(root);
+        free(root);           //libertamos o espaço da arvore
     }
  
     return 1 + freeAB(root->esq) + freeAB(root->dir);
