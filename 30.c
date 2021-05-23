1a54c332792961571b7bab83d4849626b85b2b1f
158bubbles <49231263+158bubbles@users.noreply.github.com>
2021-05-23 16:03:45 +0100

    Create 30.c

---
+++ 30.c
@@ -0,0 +1,16 @@
+void mirror (ABin *root){
+    
+    if(*root != NULL){
+        
+        //trocamos os ramos de sitio
+        ABin temporario = (*root)->esq;
+        (*root)->esq = (*root)->dir;
+        (*root)->dir = temporario;
+    }
+    
+    
+    //agora, chamamos a função recursivamente para cada ramo
+    mirror(&((*root)->esq));
+    mirror(&((*root)->dir));
+    
+}
