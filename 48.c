ca82d3b2f01b19a14737630754e2f17e3e1ef811
158bubbles <49231263+158bubbles@users.noreply.github.com>
2021-05-24 03:26:31 +0100

    Create 48.c

---
+++ 48.c
@@ -0,0 +1,21 @@
+void removeMaiorA (ABin *root) {
+    
+    ABin current = *root;
+    ABin temporario;
+    
+    if(*root == NULL){
+        return;
+    }
+    
+    //caso à direita da root for nula quer dizer q é a ultima (amaior)
+    if((*root)->dir == NULL){
+        ABin temporario = *root;   //criamos um temporario p guardar a root antes de a eliminar
+        (*root) = (*root)->esq;
+        free(temporario);
+        
+    }
+    else {
+        removeMaiorA(&((*root)->dir));
+    }
+    
+}
